f8d4be8a7b0863279a351ce93cdfb7e9a87b7242
Jack McCallum <mccallumjack@gmail.com>
2015-07-14 10:22:40 -0500

    still trying deploy

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,6 +15,3 @@
 //= require ckeditor-jquery
 //= require_tree .
 
-config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
-config.assets.precompile += Ckeditor.assets
-config.assets.precompile += %w(ckeditor/*)
