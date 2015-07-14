95626fe0bd17b3ee16e522f93c70931c8b80f095
Faithsend <matt@sandfalls.com>
2015-07-14 17:54:16 -0500

    user_for_paper_trail returns link

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,7 @@
 class ApplicationController < ActionController::Base
     def user_for_paper_trail
-      puts "$$$$$$$$$$$$$$$$"
-      puts current_member.id
-      puts "$$$$$$$$$$$$$$$$"
-      member_signed_in? ? current_member.id : 'Guest'
+
+      member_signed_in? ? "<a href='#{member_path(current_member)}'>#{current_member.email}</a>" : 'Guest'
     end
 
   protect_from_forgery with: :exception
