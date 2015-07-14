531659bc50ce79e86714ea5e6a9afc31b3ce0432
Jack McCallum <mccallumjack@gmail.com>
2015-07-14 18:31:23 -0500

    added search method

--- app/models/article.rb
+++ app/models/article.rb
@@ -4,7 +4,15 @@ class Article < ActiveRecord::Base
   validates :contents, presence: true
   validates :author, presence:true
   
-  
+
+  def self.content_search(keyword)  
+    list = {}
+    self.all.each{ |article| list[article] = article.content.scan(keyword).length }
+    list.select{ |_,value| value > 0 }.sort{|a,b| b[1]<=>a[1]}
+  end
+
+
+
   has_paper_trail
 
 end
