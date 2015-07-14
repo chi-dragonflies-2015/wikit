428745fd9bb8ba91de7aac1047aafe8922ff0b59
Faithsend <matt@sandfalls.com>
2015-07-14 17:40:28 -0500

    navigation links and such

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,10 @@
 class ApplicationController < ActionController::Base
-
-  class ApplicationController
     def user_for_paper_trail
-      logged_in? ? current_member.id : 'Guest'
+      puts "$$$$$$$$$$$$$$$$"
+      puts current_member.id
+      puts "$$$$$$$$$$$$$$$$"
+      member_signed_in? ? current_member.id : 'Guest'
     end
-  end
-
 
   protect_from_forgery with: :exception
 end
---
+++ spec/features/article_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+feature "User visits the website" do
+  scenario "when user visits the root" do
+    visit '/'
+    expect(page).to have_content("Hello World! Welcome to Wikit!")
+  end
+  scenario "when user visits the article index" do
+    Article.create()
+    visit '/index'
+    expect(page).to have_content("")
+end
