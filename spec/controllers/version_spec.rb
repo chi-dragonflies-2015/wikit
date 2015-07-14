87e9d510bd5c846f57041efb09dc7aa0b6ddd22a
Jack McCallum <mccallumjack@gmail.com>
2015-07-14 15:31:13 -0500

    finish version controller and controller specs

--- spec/controllers/version_spec.rb
+++ spec/controllers/version_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe VersionsController do
-  let!(:article) { Article.create!(title: 'Sample', contents: 'This is a sample article', author_id: 1)}
+  let!(:article) { create(:article) }
   let!(:versions) { article.versions }
 
   describe "GET #index" do
@@ -13,6 +13,22 @@ describe VersionsController do
       get :index, { article_id: article.to_param }
       expect(assigns(:versions)).to eq(versions)
     end
+  end
+
+  describe "GET #show" do
+    it "assigns @version" do
+
+      original_contents = article.contents
+      article.update(contents: "testing 2-3-4")
+      version = article.versions.last
+
+      get :show, { id: version.id, article_id: article.to_param }
+      expect(assigns(:version).reify.contents).to eq(original_contents)
+    end
+    # it "assigns versions off of article" do
+    #   get :index, { article_id: article.to_param }
+    #   expect(assigns(:versions)).to eq(versions)
+    # end
   end
 
 end
