46cb84db32423e58de98e2a3601d37266e311ca6
Faithsend <matt@sandfalls.com>
2015-07-14 08:54:37 -0500

    validations for add member and authorship on article

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,6 +2,6 @@ class Article < ActiveRecord::Base
   belongs_to :author, class_name: "Member"
 
   validates :contents, presence: true
-  # validates :author, presence:true
+  validates :author, presence:true
 
 end
