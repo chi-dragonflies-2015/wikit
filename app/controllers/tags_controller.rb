40ccc7f237d09f5257d57421b7b209f092bcfda3
Jack McCallum <mccallumjack@gmail.com>
2015-07-15 10:03:23 -0500

    add new create and index to tag controller

---
+++ app/controllers/tags_controller.rb
@@ -0,0 +1,33 @@
+class TagsController < ApplicationController
+
+  def new
+    @tag = Tag.new
+  end
+
+  def create
+    @article = Article.find_by(id: params[:article_id])
+    @tag = @article.tags.create(params[:tag])
+
+    if @tag.save
+      respond_to do |format|
+        format.html { redirect_to @article }
+        format.js
+      end
+    else
+      render 'new'
+    end
+  end
+
+
+  def index
+    @article = Article.find_by(id: params[:article_id])
+    @tags = @article.tags
+  end
+
+  private
+
+  def tag_params
+    params.require(:tag).permit(:name)
+  end
+
+end
