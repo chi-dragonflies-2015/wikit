e9f982c2b593a21162e07e14028b61ccf78fe709
Henry Firth <henrynf12@gmail.com>
2015-07-15 14:13:15 -0500

    Add featured article check box to edit page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -46,6 +46,6 @@ class ArticlesController < ApplicationController
     end
 
     def article_params
-      params.require(:article).permit(:title, :contents)
+      params.require(:article).permit(:title, :contents, :featured, :state)
     end
 end
