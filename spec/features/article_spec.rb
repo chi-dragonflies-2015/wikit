f4b940eb81fa20500821e076d4e39bd2de945fa1
Henry Firth <henrynf12@gmail.com>
2015-07-15 14:09:19 -0500

    Add tests for featured button on edit page

--- spec/features/article_spec.rb
+++ spec/features/article_spec.rb
@@ -1,13 +1,37 @@
 require 'rails_helper'
+include Warden::Test::Helpers
+Warden.test_mode!
 
 feature "User visits the website" do
   scenario "when user visits the root" do
     visit '/'
     expect(page).to have_content("Hello World! Welcome to Wikit!")
   end
-  pending "when user visits the article index" do
-    Article.create()
-    visit '/index'
-    expect(page).to have_content("")
+end
+
+feature "Homepage shows links to featured articles" do
+  scenario "when there exist featured articles" do
+    featured_article = Article.create(title: "Sample title", contents: "Sample contents", author: Member.create(email: "test@test.com", password: "password"), featured: true)
+    visit '/'
+    find_link(featured_article.title).visible?
+  end
+end
+
+feature "Admins can feature or un-feature articles" do
+  before(:each) do
+    admin = FactoryGirl.create(:member)
+    admin = admin.
+    login_as(admin , :scope => :member)
+  end
+
+  scenario "when an admin looks at an un-featured article's show page" do
+    unfeatured_article = Article.create(title: "Sample title", contents: "Sample contents", author: Member.create(email: "test@test.com", password: "password"))
+    visit "/articles/#{unfeatured_article.id}/edit"
+    page.uncheck('article_featured')
+  end
+  scenario "when an admin looks at a featured article's show page" do
+    featured_article = Article.create(title: "Sample title", contents: "Sample contents", author: Member.create(email: "test@test.com", password: "password"), featured: true)
+    visit "/articles/#{featured_article.id}/edit"
+    page.check('article_featured')
   end
 end
