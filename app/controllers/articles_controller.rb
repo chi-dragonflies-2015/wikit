69548ed00f5568c2145c16b8a2a9711c2b796a47
Jack McCallum <mccallumjack@gmail.com>
2015-07-14 19:20:46 -0500

    added search check to index controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,7 @@ class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :destroy, :edit, :update]
 
   def index
-    @articles = params[:search_keyword] ? Article.content_search(params[:search_keyword]) : Article.all
+    @articles = params[:search] ? Article.content_search(params[:search]) : Article.all
   end
 
   def show
