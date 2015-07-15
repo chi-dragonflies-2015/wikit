430cbfa65eabcb86751b1b010efc566715e1c6e5
Faithsend <matt@sandfalls.com>
2015-07-15 10:07:16 -0500

    add support for login to check on article routes

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,9 +5,10 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'spec_helper'
 require 'rspec/rails'
-require 'capybara/rails'
-# Add additional requires below this line. Rails is not loaded until this point!
 
+# Add additional requires below this line. Rails is not loaded until this point!
+require 'capybara/rails'
+require 'devise'
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
@@ -51,4 +52,6 @@ RSpec.configure do |config|
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
   config.include Devise::TestHelpers, type: :controller
+  config.extend ControllerMacros, :type => :controller
+
 end
