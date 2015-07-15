0c64fc9a782220ca20783c1f41c3095d3f4f9f9c
Faithsend <matt@sandfalls.com>
2015-07-14 23:31:55 -0500

    spec for delete buttons and rearranged things

--- spec/features/article_spec.rb
+++ spec/features/article_spec.rb
@@ -5,11 +5,9 @@ feature "User visits the website" do
     visit '/'
     expect(page).to have_content("Hello World! Welcome to Wikit!")
   end
-  pending "when user visits the article index" do
-    Article.create()
-    visit '/index'
-    expect(page).to have_content("")
-  end
+end
+
+feature "search capability!" do
   scenario "when user searches for something" do
     Article.create(title: "Example Title Green", contents: "a fish called wanda", author: build(:author) )
     Article.create(title: "Example Title Purple", contents: "a fish called harold", author: build(:author))
@@ -21,4 +19,26 @@ feature "User visits the website" do
   end
 
   # Maybe write one to show that they are in the proper order?
+end
+
+feature "delete buttons!" do
+  scenario "when user visits the article index as a member" do
+    article = Article.create(title: "Example Title Green", contents: "a fish called wanda", author: build(:author) )
+    member = FactoryGirl.create(:member)
+    login_as(member, :scope => :member)
+    visit '/articles'
+    expect(page).to have_content(article.title)
+    expect(page.has_link?("delete")).to be false
+  end
+
+  scenario "when user visits the article index as an admin" do
+    article = Article.create(title: "Example Title Green", contents: "a fish called wanda", author: build(:author) )
+    member = FactoryGirl.create(:member)
+    member.admin = true
+    member.save
+    login_as(member, :scope => :member)
+    visit '/articles'
+    expect(page).to have_content(article.title)
+    expect(page.has_link?("delete")).to be true
+  end
 end
