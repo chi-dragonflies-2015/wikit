fb310f1c39af226b766b89bf63f11eecb4bf74dc
Henry Firth <henrynf12@gmail.com>
2015-07-14 19:00:58 -0500

    Add default values to state and featured

--- db/migrate/20150714231929_add_state_to_articles.rb
+++ db/migrate/20150714231929_add_state_to_articles.rb
@@ -1,5 +1,5 @@
 class AddStateToArticles < ActiveRecord::Migration
   def change
-    add_column :articles, :state, :string
+    add_column :articles, :state, :string, :default => 'unpublished'
   end
 end
